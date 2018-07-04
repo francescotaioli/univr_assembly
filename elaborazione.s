67eb948edb4bd65cf94b433d72ea0360429f6e14
Francesco <taioli.francesco98@gmail.com>
2018-07-04 16:21:20 +0200

    add elabroazione

---
+++ elaborazione.s
@@ -0,0 +1,2 @@
+#questo file si occupa di ricevere come input 'bufferin' e deve restituire una variabile stringa 'bufferout_asm'
+#qui verrà tutta l'esecuzione del programma che chiamera anche altri file
