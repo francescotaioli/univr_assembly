0c2a864cc76a13d5d147f494f6b65775114e90ae
giuseppetanello <giuseppe.tanello@gmail.com>
2018-07-04 16:55:34 +0200

    test .s

--- elaborazione.s
+++ elaborazione.s
@@ -1,20 +1,67 @@
-#questo file si occupa di ricevere come input 'bufferin' e deve restituire una variabile stringa 'bufferout_asm'
-#qui verrà tutta l'esecuzione del programma che chiamera anche altri file
+# filename: num2str.s
+#
+# converts an integer into a string
 
-.data
-.text
-    .global pot_asm # funzione deve essere globale
-pot_asm:
-    pushl %ebp # event. chiamate nidificate
-    movl %esp, %ebp # imposto %ebp
-    movl 8(%ebp), %ebx # leggo la base
-    movl 12(%ebp), %ecx # leggo l’esponente
-    xorl %edx, %edx # azzero %edx
-    movl $1, %eax # imposto accumulatore
+.section .data
 
-ciclo:
-    mull %ebx # %eax = %eax*%ebx
-    loopl ciclo # ciclo %ecx volte
-    # al termine il ris si trova in %eax o in
-    popl %ebp # ripristino %ebp precedente
-ret
+	numstr: .ascii "00000000000"     # string output
+
+	numtmp: .ascii "00000000000"     # temporary string
+
+
+.section .text
+	.global _start
+
+_start:
+
+	movl $12345, %eax          # carica il valore da convertire in EAX
+
+	movl $10, %ebx             # carica 10 in EBX (usato per le divisioni)
+	movl $0, %ecx              # azzera il contatore ECX
+
+	leal numtmp, %esi          # carica l'indirizzo di 'numtmp' in ESI
+
+
+continua_a_dividere:
+
+	movl $0, %edx              # azzera il contenuto di EDX
+	divl %ebx                  # divide per 10 il numero ottenuto
+
+	addb $48, %dl              # aggiunge 48 al resto della divisione
+	movb %dl, (%ecx,%esi,1)    # sposta il contenuto di DL in numtmp
+
+	inc %ecx                   # incrementa il contatore ECX
+
+	cmp $0, %eax               # controlla se il contenuto di EAX è 0
+
+	jne continua_a_dividere
+
+
+	movl $0, %ebx              # azzera un secondo contatore in EBX
+
+	leal numstr, %edx          # carica l'indirizzo di 'numstr' in EDX
+
+ribalta:
+
+	movb -1(%ecx,%esi,1), %al  # carica in AL il contenuto dell'ultimo byte di 'numtmp'
+	movb %al, (%ebx,%edx,1)    # carica nel primo byte di 'numstr' il contenuto di AL
+
+	inc %ebx                   # incrementa il contatore EBX
+
+	loop ribalta
+
+
+stampa:
+
+	movb $10, (%ebx,%edx,1)    # aggiunge il carattere '\n' in fondo a 'numstr'
+
+	inc %ebx
+	movl %ebx, %edx            # carica in EDX la lunghezza della stringa 'numstr'
+	movl $4, %eax              # carica in EAX il codice della syscall WRITE
+	movl $1, %ebx              # carica in EBX il codice dello standard output
+	leal numstr, %ecx          # carica in ECX l'indirizzo della stringa 'numstr'
+	int $0x80                  # esegue la syscall
+
+	movl $1, %eax              # carica in EAX il codice della syscall EXIT
+	movl $0, %ebx              # carica in EBX il codice di uscita 0
+	int $0x80                  # esegue la syscall
