625fc29d889e14bd0ef9f4208266fae275de8ff6
giuseppetanello <giuseppe.tanello@gmail.com>
2018-07-06 17:01:30 +0200

    altro metodo per scrivere len

--- elaborazione.s
+++ elaborazione.s
@@ -21,6 +21,8 @@ asm_main:
     movl $1, %ebx      				#scelgo il terminale
     movl 8(%ebp), %ecx      		#mette la stringa in ecx
 
+    addl $1, %ecx
+
     movl riga_len, %edx     		#metto la lunghezza in edx
 
     int $0x80 						#interrupt print
