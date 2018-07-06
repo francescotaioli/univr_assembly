9ec563a81f38643de8ddec9e284984590fc23bdf
Francesco <taioli.francesco98@gmail.com>
2018-07-06 15:38:18 +0200

    add

--- elaborazione.s
+++ elaborazione.s
@@ -17,7 +17,8 @@ asm_main:
     movl 4(%esp), %ecx 					#mette la stringa in ecx
     movl riga_len, %edx 				#metto la lunghezza in edx
 
-    int  $0x80 #interrupt print
-	movl  %ebp, %esp
-	popl  %ebp
+    int  $0x80 							#interrupt print
+	
+	movl  %ebp, %esp					#ripristino lo stack
+	popl  %ebp							#ripristino lo stack
     ret
