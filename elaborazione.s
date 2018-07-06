f61ebcad5f7a082d87a847ad4beff740deb86c69
Francesco <taioli.francesco98@gmail.com>
2018-07-06 16:48:31 +0200

    add

--- elaborazione.s
+++ elaborazione.s
@@ -13,11 +13,16 @@ asm_main:
 
     pushl %ebp
 	movl %esp, %ebp
-
+	movl 8(%ebp), %esi
     movl $4, %eax      				#syscall print
     movl $1, %ebx      				#scelgo il terminale
-    movl 8(%ebp), %ecx      		#mette la stringa in ecx
-    movl riga_len, %edx     		#metto la lunghezza in edx
+movl $5, -12(%ebp, %ebx, 4)
+	movl 8(%ebp), %ecx      		#mette la stringa in ecx
+	movl  (%esi),%eax  		#; read the first element
+	movl 4(%esi ),%eax 		#; read the second eleme
+	movl 8(%esi ),%eax
+	
+	movl riga_len, %edx     		#metto la lunghezza in edx
 
     int  $0x80 						#interrupt print
 	movl  %ebp, %esp
