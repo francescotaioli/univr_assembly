04ef0f5472fe3e49acbaae8909d8be87d8c0a876
Francesco <taioli.francesco98@gmail.com>
2018-07-06 15:57:36 +0200

    add

--- elaborazione.s
+++ elaborazione.s
@@ -1,4 +1,3 @@
-
 .section .data
 
 riga:
@@ -13,14 +12,14 @@ riga_len:
 asm_main:
 
     pushl %ebp
-    movl %esp, %ebp
+	movl %esp, %ebp
 
-    movl $4, %eax						#syscall print
-    movl $1, %ebx						#scelgo il terminale
-    movl 4(%esp), %ecx 					#mette la stringa in ecx
-    movl riga_len, %edx 				#metto la lunghezza in edx
+    movl $4, %eax      				#syscall print
+    movl $1, %ebx      				#scelgo il terminale
+    movl 8(%esp), %ecx      		#mette la stringa in ecx
+    movl riga_len, %edx     		#metto la lunghezza in edx
 
-    int  $0x80 #interrupt print
-
-	popl  %ebp
-	ret
+    int  $0x80 						#interrupt print
+	movl  %ebp, %esp
+ 	popl  %ebp
+ ret
