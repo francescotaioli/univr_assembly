e07f930cb1bdeb96cf4cbf3982c28876ea872fdf
Francesco <taioli.francesco98@gmail.com>
2018-07-06 15:24:00 +0200

    change comment

--- elaborazione.s
+++ elaborazione.s
@@ -11,12 +11,10 @@ riga_len:
 
 asm_main:
 
-    movl $4, %eax
-    movl $1, %ebx
-    #mette mega stringona in ecx
-    movl 4(%esp), %ecx
-    #stampa la mega stringona per la lunghezza di riga
-    movl riga_len, %edx
+    movl $4, %eax						#syscall print
+    movl $1, %ebx						#scelgo il temrminale
+    movl 4(%esp), %ecx 					#mette la stringa in ecx
+    movl riga_len, %edx 				#metto la linghezza in edx
 
-    int  $0x80
+    int  $0x80 #interupt print
     ret
