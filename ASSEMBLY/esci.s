aefa4e96694cb3fe337fb2bcf62ccb4cece0efde
Taioli Francesco <taioli.francesco98@gmail.com>
2018-07-31 16:35:49 +0200

    delete some file

--- ASSEMBLY/esci.s
+++
@@ -1,9 +0,0 @@
-.section .text
-.global esci
-.type esci, @function
-
-esci:
-	movl $1,   %eax
-	xorl %ebx, %ebx
-	int	 $0x80
-ret
--- runAll.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-rm -f *.o
-gcc -m32 -g -o exe elaborazione.s controllore.c
-./exe testin.txt prova_OUT
